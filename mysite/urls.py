8066a0ae116d1314035c48c29b30402bb49d2efd
Nirmal-coder <nirmaladhikari@na.local>
2021-06-21 19:21:55 +0545

    Changed home HTML for first time

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# lines between the triple quotes are called docstrings. They wont be run by python. They are there only for the description of file
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
 ]
